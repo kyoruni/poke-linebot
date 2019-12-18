5821e6f073476b22cf23cea36402bd3cc24ff533
kyoruni <kyoruni@gmail.com>
2019-12-19 00:44:20 +0900

    :wrench: 複数匹の種族値を返せるようにした

--- statusbot.php
+++ statusbot.php
@@ -11,45 +11,102 @@ function load_json($url)
 }
 
 // 存在チェック
-function check_exist($search, $items, $column)
+function check_exist($input, $pokemons)
 {
-    return in_array($search, array_column($items, $column));
+    return in_array($input, array_column($pokemons, 'name'));
 }
 
-// // 環境変数読み込み
-// $dotenv = Dotenv::create(__DIR__);
-// $dotenv->load();
-// $accessToken = $_ENV["LINE_KEY"];
+class Pokemon
+{
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
 
-// //ユーザーからのメッセージ取得
-// $json_string = file_get_contents('php://input');
-// $jsonObj     = json_decode($json_string);
+    // タイプと種族値を取得
+    public function get_types_and_status($pokemons) {
+        foreach($pokemons as $pokemon) {
+            if ($this->name === $pokemon['name']) {
+                $this->types  = $pokemon['types'];
+                $this->status = $pokemon['status'];
+            }
+        }
+    }
 
-// //ユーザーからのメッセージ取得
-// $json_string = file_get_contents('php://input');
-// $jsonObj     = json_decode($json_string);
+    // タイプをカンマ区切りの文字列にする
+    public function trim_types() {
+        $this->types = implode(",", $this->types);
+    }
 
-// //メッセージ取得
-// $type  = $jsonObj->{"events"}[0]->{"message"}->{"type"};
-// $input = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+    // 出力する形に整形
+    public function set_result() {
+        $text  = "--------------------\n";
+        $text .= "【{$this->name}】";
+        $text .= $this->types . PHP_EOL;
+        $text .= "H[{$this->status['h']}] ";
+        $text .= "A[{$this->status['a']}] ";
+        $text .= "B[{$this->status['b']}] ";
+        $text .= "C[{$this->status['c']}] ";
+        $text .= "D[{$this->status['d']}] ";
+        $text .= "S[{$this->status['s']}] " . PHP_EOL;
+        $this->result = $text;
+    }
+}
 
-// //ReplyToken取得
-// $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
+// 結果を返す
+function return_post($text, $replyToken, $accessToken)
+{
+    $response_format_text = [
+        "type" => "text",
+        "text" => $text,
+    ];
+    $post_data = [
+        "replyToken" => $replyToken,
+        "messages"   => [$response_format_text],
+    ];
+    $curl = curl_init("https://api.line.me/v2/bot/message/reply");
+    curl_setopt_array($curl, [
+        CURLOPT_POST           => true,
+        CURLOPT_CUSTOMREQUEST  => 'POST',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POSTFIELDS     => json_encode($post_data),
+        CURLOPT_HTTPHEADER     => array(
+            'Content-Type: application/json; charser=UTF-8',
+            'Authorization: Bearer ' . $accessToken)
+    ]);
+    return $curl;
+}
 
-// //メッセージ以外のときは何も返さず終了
-// if ($type != "text") {
-//   exit;
-// }
+// 環境変数読み込み
+$dotenv = Dotenv::create(__DIR__);
+$dotenv->load();
+$accessToken = $_ENV["LINE_KEY"];
+
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$jsonObj     = json_decode($json_string);
+
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$jsonObj     = json_decode($json_string);
+
+//メッセージ取得
+$type        = $jsonObj->{"events"}[0]->{"message"}->{"type"};
+$input       = $jsonObj->{"events"}[0]->{"message"}->{"text"};
+
+//ReplyToken取得
+$replyToken  = $jsonObj->{"events"}[0]->{"replyToken"};
+
+//メッセージ以外のときは何も返さず終了
+if ($type != "text") {
+    exit;
+}
 
 // jsonファイル読み込み
-// データ読み込み
 $pokemons = load_json("json/pokemon.json");
-$types    = load_json("json/pokemon_type.json");
-
-$input = "ピカチュウ、ギャラドス";
 
 if (strpos($input,"、") !== false) {
-    // ユーザーからのメッセージに、「、」が含まれていたら二匹
+    // ユーザーからのメッセージに、「、」が含まれていたら複数
     $check_pokemons = explode("、", $input);
 } else {
     // ユーザーからのメッセージに、「、」が含まれていなかったら1匹
@@ -58,11 +115,32 @@ if (strpos($input,"、") !== false) {
 
 // 存在チェック
 foreach ($check_pokemons as $check_pokemon) {
-    if (check_exist($check_pokemon, $pokemons, 'name') == false) {
-      $text = "該当するポケモンが見つかりません…";
-      exit;
-      // $curl   = return_post($text, $replyToken, $accessToken);
-      // $result = curl_exec($curl);
-      // curl_close($curl);
+    if (check_exist($check_pokemon, $pokemons) === false) {
+        $text = "該当するポケモンが見つかりません…";
+        $curl   = return_post($text, $replyToken, $accessToken);
+        $result = curl_exec($curl);
+        curl_close($curl);
+        exit;
     }
 }
+
+$i = 1;
+$result_text = "";
+foreach ($check_pokemons as $check_pokemon) {
+    $pokemon    = 'pokemon' . $i;
+    ${$pokemon} = new Pokemon($check_pokemon);
+
+    // タイプと種族値を取得
+    ${$pokemon}->get_types_and_status($pokemons);
+    ${$pokemon}->trim_types();
+
+    // 出力する形に整形
+    ${$pokemon}->set_result();
+
+    // 結果を返す
+    $result_text .= ${$pokemon}->result;
+    $i ++;
+}
+$curl   = return_post($result_text, $replyToken, $accessToken);
+$result = curl_exec($curl);
+curl_close($curl);
